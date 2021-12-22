7200abaa487c324b137e6a5bda086fb52a9ab449
Chris Cherniakov <taequn@gmail.com>
2021-12-22 16:35:26 -0500

    Multiprocessing+refractor for the script

--- emailValidity/emailValidity.py
+++ emailValidity/emailValidity.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import validate_email
 import pandas as pd
 
@@ -5,7 +6,7 @@ import pandas as pd
 #OUTPUT: dataframe
 #DESCRIPTION: Checks the csv file for valid email columns
 #			  Accepted filetype: csv, tsv,xlsx
-def checkCSV(filename, type):
+def checkCSV(filename, type, debug=False, multi=False):
 	if(type == "csv"):
 		try:
 			df = pd.read_csv(filename)
@@ -19,54 +20,102 @@ def checkCSV(filename, type):
 	else:
 		raise Exception("Invalid file type")
 
+	#refractor this code
 	if "email" in df.columns:
-		return df
+		pass
+	elif "Email(s)" in df.columns:
+		df.rename(columns={"Email(s)":"email"}, inplace=True)
 	elif "Email" in df.columns:
 		df.rename(columns={"Email":"email"}, inplace=True)
-		return df
-
 	else:
 		raise Exception("Column not found")
+
+	if multi:
+		return multiprocess(df, debug)
+	else:
+		return checkTheMail(df, debug)
 
 #INPUT: filename, debug, type
 #OUTPUT: updated dataframe
 #DESCRIPTION: Goes through the csv file and checks the email column for valid emails
 #			  Removes invalid emails and returns a dataframe
-def checkTheMail(filename, type, debug):
-	df = checkCSV(filename, type)
-
+def checkTheMail(df, debug=True):
+	total = len(df)
 	for i in range(0, len(df["email"])):
+		currLength = len(df["email"])
 		if debug:
-			print(i, len(df["email"]))
+			print("Checked ", i+1, "out of ", total, "(current length: ", currLength, ")")
 		try:
 			validate_email.validate_email_or_fail(
 								email_address=df["email"][i],
 								check_format=True,
 								check_dns=True,
-								check_smtp=True)
+								check_smtp=True,
+								smtp_from_address="xakel6@gmail.com")
 		except Exception as e:
-			if(debug):
-				print(e)
-			df.drop(i, inplace=True)
-
+			if "Hui" in str(e):
+				continue
+			else:
+				if(debug):
+					print(e)
+				df.drop(i, inplace=True)
 	return df
 
 #INPUT: filename, debug, type
 #OUTPUT: saved dataframe
 #DESCRIPTION: Save the dataframe to a csv file
 # 			  Use type to specify the file type
-def checkAndSave(filename, type, debug=False):
-	df = checkTheMail(filename, type, debug)
+def checkAndSave(filename, type, debug=True, multi=False):
+	df = checkCSV(filename, type, debug, multi)
 	filename = filename.split(".")
-	df.to_csv(filename[0]+"_updated.csv", index=False)
+	df.to_csv(filename[0]+"_clean.csv", index=False)
 
 #INPUT: filename, debug, type
 #OUTPUT: dataframe
 #DESCRIPTION: Returns the checked dataframe
 # 			  Use type to specify the file type
-def checkAndShow(filename, type, debug=False):
-	return checkTheMail(filename, type, debug)
+def checkAndShow(filename, type, debug=True, multi=False):
+	return checkCSV(filename, type, debug, multi)
+
+def multiprocess(df, debug=True):
+	#split dataframe into 4 parts with new index
+	df1 = df.iloc[0:len(df)//4]
+	df2 = df.iloc[len(df)//4:len(df)//2]
+	df3 = df.iloc[len(df)//2:len(df)*3//4]
+	df4 = df.iloc[len(df)*3//4:len(df)]
+
+	#reindex the dataframes
+	df1.index = range(0, len(df1))
+	df2.index = range(0, len(df2))
+	df3.index = range(0, len(df3))
+	df4.index = range(0, len(df4))
+
+	#create threads
+	t1 = Thread(target=checkTheMail, args=(df1, debug))
+	t2 = Thread(target=checkTheMail, args=(df2, debug))
+	t3 = Thread(target=checkTheMail, args=(df3, debug))
+	t4 = Thread(target=checkTheMail, args=(df4, debug))
+
+	#start threads
+	t1.start()
+	t2.start()
+	t3.start()
+	t4.start()
+
+	#join threads
+	t1.join()
+	t2.join()
+	t3.join()
+	t4.join()
+
+	#combine dataframes
+	df = pd.concat([df1, df2, df3, df4])
+	df.index = range(0, len(df))
+
+	#save dataframe
+	return df
 
 
 if __name__ == '__main__':
-	checkAndSave("test.csv", debug=True, type="csv")
+	print(checkAndShow("ChoiceNYoutlets.csv", "csv", True, True))
+
