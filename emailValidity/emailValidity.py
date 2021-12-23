41f86960872bf72501a5ba6865b253be1771b065
Aleksei Rutkovskii <arutkovskii@umass.edu>
2021-12-23 16:26:40 +0300

    working version without threading

--- emailValidity/emailValidity.py
+++ emailValidity/emailValidity.py
@@ -1,78 +1,182 @@
+from threading import Thread
 import validate_email
 import pandas as pd
 
-#INPUT: filename, type
-#OUTPUT: dataframe
-#DESCRIPTION: Checks the csv file for valid email columns
-#			  Accepted filetype: csv, tsv,xlsx
-def checkCSV(filename, type):
-	if(type == "csv"):
-		try:
-			df = pd.read_csv(filename)
-		except:
-			raise Exception("File not found")
-	elif(type == "xlsx"):
-		try:
-			df = pd.read_excel(filename)
-		except:
-			raise Exception("File not found")
-	else:
-		raise Exception("Invalid file type")
 
-	if "email(s)" in df.columns:
-		return df
-	elif "Email(s)" in df.columns:
-		df.rename(columns={"Email(s)":"email"}, inplace=True)
-		return df
+class emailValidation:
+	# Constructor
+	# @param: filename — path, type — csv/xlsx, multi — multi-threading for faster processing
+	def __init__(self, filename=None, type=None, debug=False, multi=False):
+		self.filename = filename
+		self.type = type
+		self.debug = debug
+		self.multi = multi
+		self.df = None
 
-	else:
-		raise Exception("Column not found")
+		self.checkCSV()
 
-#INPUT: filename, debug, type
-#OUTPUT: updated dataframe
-#DESCRIPTION: Goes through the csv file and checks the email column for valid emails
-#			  Removes invalid emails and returns a dataframe
-def checkTheMail(filename, type, debug):
-	df = checkCSV(filename, type)
+	# Check if the file is a csv or xlsx
+	# @return: Validates the file or throws an error
+	def checkCSV(self):
+		if self.type == "csv":
+			try:
+				df = pd.read_csv(self.filename)
+			except:
+				raise Exception("File not found")
+		elif self.type == "xlsx":
+			try:
+				df = pd.read_excel(self.filename)
+			except:
+				raise Exception("File not found")
+		else:
+			raise Exception("Invalid file type")
 
-	for i in range(0, len(df["email"])):
-		if debug:
-			print(i, len(df["email"]))
-		try:
-			validate_email.validate_email_or_fail(
-								email_address=df["email"][i],
-								check_format=True,
-								check_dns=True,
-								check_smtp=True)
-		except Exception as e:
-			if(debug):
-				print(e)
-			df.drop(i, inplace=True)
+		# Change this code
+		if "email" in df.columns:
+			df.rename(columns={"email": "Email(s)"}, inplace=True)
+		elif "Email(s)" in df.columns:
+			pass
+		elif "Email" in df.columns:
+			df.rename(columns={"Email": "Email(s)"}, inplace=True)
+		else:
+			raise Exception("Column not found")
 
-	return df
+		self.df = df
 
-#INPUT: filename, debug, type
-#OUTPUT: saved dataframe
-#DESCRIPTION: Save the dataframe to a csv file
-# 			  Use type to specify the file type
-def checkAndSave(filename, type, debug=False):
-	df = checkTheMail(filename, type, debug)
-	filename = filename.split(".")
-	df.to_csv(filename[0]+"_updated.csv", index=False)
+	# Validate the email
+	# @param: data — either .self.df or a list of emails
+	# @return: Validates the email or removes it from the dataframe
+	def checkTheMail(self, data=None):
+		if data is None:
+			data = self.df
+		total = len(data)
+		for i in range(0, len(data["email"])):
+			currLength = len(data["email"])
+			if self.debug:
+				print("Checked ", i+1, "out of ", total, "(current length: ", currLength, ")")
+			try:
+				validate_email.validate_email_or_fail(
+									email_address=data["email"][i],
+									check_format=True,
+									check_dns=True,
+									check_smtp=True,
+									smtp_from_address="xakel6@gmail.com")
+			except Exception as e:
+				if "Hui" in str(e):
+					continue
+				else:
+					if self.debug:
+						print(e)
+					data.drop(i, inplace=True)
 
+		if self.multi:
+			return data
+		else:
+			self.df = data
+
+	# Validate the emails in a thread
+	# @return: Validates the emails or throws an error
+	def multiprocess(self):
+		# split dataframe into 4 parts with new index
+		initial = len(self.df)
+		df1 = self.df.iloc[0:initial//4]
+		df2 = self.df.iloc[initial//4:initial//2]
+		df3 = self.df.iloc[initial//2:initial*3//4]
+		df4 = self.df.iloc[initial*3//4:initial]
+
+		# reindex the dataframes
+		df1.index = range(0, len(df1))
+		df2.index = range(0, len(df2))
+		df3.index = range(0, len(df3))
+		df4.index = range(0, len(df4))
+
+		# create threads
+		t1 = Thread(target=self.checkTheMail, args=(df1,))
+		t2 = Thread(target=self.checkTheMail, args=(df2,))
+		t3 = Thread(target=self.checkTheMail, args=(df3,))
+		t4 = Thread(target=self.checkTheMail, args=(df4,))
+
+		# start threads
+		t1.start()
+		t2.start()
+		t3.start()
+		t4.start()
+
+		# join threads
+		t1.join()
+		print("********************************************************")
+		print("THREAD 1 COMPLETE")
+		print("********************************************************")
+		t2.join()
+		print("********************************************************")
+		print("THREAD 2 COMPLETE")
+		print("********************************************************")
+		t3.join()
+		print("********************************************************")
+		print("THREAD 3 COMPLETE")
+		print("********************************************************")
+		t4.join()
+		print("********************************************************")
+		print("THREAD 4 COMPLETE")
+		print("********************************************************\n")
+
+		# combine dataframes
+		df = pd.concat([df1, df2, df3, df4])
+		df.index = range(0, len(df))
+		print("Initial length: ", initial)
+		print("Final length: ", len(df))
+		self.df = df
+
+	# Initiates the checks
+	# @param: save — save the dataframe to a csv
+	# @return: cleaned dataframe
+	def check(self, save=False):
+		if self.multi:
+			self.multiprocess()
+		else:
+			self.checkTheMail()
+
+		if save:
+			filename = self.filename.split(".")
+			self.df.to_csv(filename[0]+"_clean.csv", index=False)
+			return self.df
+		else:
+			return self.df
+
+
+	def setFilename(self, filename):
+		if type(filename) is str:
+			self.filename = filename
+		else:
+			raise Exception("Invalid file type")
+
+	def setType(self, type):
+		if type == "csv" or type == "xlsx":
+			self.type = type
+		else:
+			raise Exception("Invalid file type")
+
+	def setMulti(self, multi):
+		if type(multi) is bool:
+			self.multi = multi
+		else:
+			raise Exception("Invalid file type")
+
+	def setDebug(self, debug):
+		if type(debug) is bool:
+			self.debug = debug
+		else:
+			raise Exception("Invalid file type")
+
+
+if __name__ == '__main__':
+	valid = emailValidation(filename="test.csv", type="csv", debug=True, multi=True)
+	valid.check(save=True)
+
+"""
 def checkAndSave2(filepath,safepath, type, debug=False):
 	df = checkTheMail(filepath, type, debug)
 	filename = filepath.split("/")[-1]
 	filename = filename.split(".")[0]
 	df.to_csv(safepath+filename+"_updated.csv", index=False)
-
-#INPUT: filename, debug, type
-#OUTPUT: dataframe
-#DESCRIPTION: Returns the checked dataframe
-# 			  Use type to specify the file type
-def checkAndShow(filename, type, debug=False):
-	return checkTheMail(filename, type, debug)
-
-
-if __name__ == '__main__':
-	checkAndSave("test.csv", debug=True, type="csv")
+"""
