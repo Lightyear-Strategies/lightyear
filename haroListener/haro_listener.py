1986aa22b78340d55134e23c7ca64be76564140a
Aleksei Rutkovskii <arutkovskii@umass.edu>
2021-12-28 23:24:29 +0300

    updating .gitignore

--- haroListener/haro_listener.py
+++ haroListener/haro_listener.py
@@ -40,7 +40,7 @@ class HaroListener():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    './config/client.json', self.scopes)
+                    'client.json', self.scopes)
                 creds = flow.run_local_server(port=0)
             with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
