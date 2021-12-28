206c0ba8ebd57d5f30d78d126651b65cb46d551e
Chris Cherniakov <taequn@gmail.com>
2021-12-28 21:00:11 +0300

    adding celery

--- flask/flaskMain.py
+++ flask/flaskMain.py
@@ -3,13 +3,16 @@
 from flask import Flask, render_template,request,redirect,url_for,session,flash
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
-from utils import *
+from utils import * #imports celery
 import emailReport
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 from flask_wtf.file import FileField, FileAllowed
 from flask_sqlalchemy import SQLAlchemy
+
+# from celery import Celery
+
 
 import os
 import sys
@@ -25,12 +28,21 @@ ALLOWED_EXTENSIONS = {'.csv','.xlsx'}
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__,template_folder='HTML')
+
 app.secret_key = "super secret key"
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.config['CELERY_BROKER_URL'] = 'amqp://guest:guest@localhost:5672/' #'amqp://guest:guest@http://127.0.0.1:5000/' #rabbit mq.
+#app.config['CELERY_BACKEND'] = #db
+celery = make_celery(app)
+
 bootstrap = Bootstrap(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -44,6 +56,8 @@ def index():
         email = form.email.data
         file = form.file.data
     form.email.data = ''
+
+
     if file:
         print(email)
         #save the file
@@ -54,26 +68,66 @@ def index():
         #find extension
         extension = os.path.splitext(filename)[1]
         extension = "csv" if extension == ".csv" else "xlsx"
+
+        """Celery"""
         #parse
-        emailVerify(os.path.join(app.config['UPLOAD_FOLDER'], filename), email, extension)
+        go.delay(os.path.join(app.config['UPLOAD_FOLDER'], filename), email, extension)
+        """
+        emailVerify.(os.path.join(app.config['UPLOAD_FOLDER'], filename), email, extension)
+
         #remove the file
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File {0} uploaded and parsed!\nInitial length: {1}. Final length: {2}'.format(filename,
                                                                                             session["initial"],
-                                                                                            session["final"]))
+                                                                                           session["final"]))
+        """
         return redirect("/")
 
     return render_template('upload.html', form=form, email=email, file=file)
 
+#
+@celery.task(name='flaskMain.go')
+def go(path, recipients=None, extension="csv"):
+    emailVerify(path, recipients, extension)
+
+    # remove the file
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    flash('File {0} uploaded and parsed!\nInitial length: {1}. Final length: {2}'.format(filename,
+                                                                                         session["initial"],
+                                                                                         session["final"]))
+
+# processSheet
+"""
+@celery.task(name='flaskMain.processSheet')
+def processSheet(file, email):
+    print(email)
+    # save the file
+    filename = secure_filename(file.filename)
+    # determine the extension
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # find extension
+    extension = os.path.splitext(filename)[1]
+    extension = "csv" if extension == ".csv" else "xlsx"
+    # parse
+    emailVerify(os.path.join(app.config['UPLOAD_FOLDER'], filename), email, extension)
+    # remove the file
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    flash('File {0} uploaded and parsed!\nInitial length: {1}. Final length: {2}'.format(filename,
+                                                                                         session["initial"],
+                                                                                         session["final"]))
+    #return 'done' #redirect("/")
+"""
+
 #class for the form
 class NameForm(FlaskForm):
-    email = StringField('What is your name?', validators=[DataRequired(), Email()])
+    email = StringField('What is your email?', validators=[DataRequired(), Email()])
     file = FileField('Select your file', validators=[DataRequired(),
                                                      FileAllowed(["csv", "xlsx"],
                                                                  "Only CSV or XLSX files are allowed")])
     submit = SubmitField('Submit')
 
-
+#@celery.task(name='flaskMain.emailVerify')
 def emailVerify(path, recipients=None, extension="csv"):
     valid = emailValidity.emailValidation(filename=path,type=extension, debug=True, multi=True)
     valid.check(save=True, inplace=True)
@@ -81,7 +135,7 @@ def emailVerify(path, recipients=None, extension="csv"):
     session["final"]=valid.getFinalLength()
     subjectLine = os.path.basename(path)
 
-    report = emailReport.report("chris@lightyearstrategies.com", recipients,
+    report = emailReport.report("aleksei@lightyearstrategies.com", recipients,
                                 "Checked '%s' file" % subjectLine, "We parsed your file!", path,
                                 "me")
     report.sendMessage()
