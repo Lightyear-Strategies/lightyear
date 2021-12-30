4bf9d06175065759084bfe54bf62b8945bf363ae
Chris Cherniakov <taequn@gmail.com>
2021-12-30 15:08:48 -0500

    Muckrack parser

--- muckRack/muckrack.py
+++ muckRack/muckrack.py
@@ -1,38 +1,63 @@
 from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
+import pickle
 import time
+import undetected_chromedriver.v2 as uc
+import pandas as pd
 
 class Muckrack:
-    def __init__(self, url):
-        self.url = url
-        self.soup = None
+    def __init__(self, url_list):
+        self.url_list = []
+        self.df = pd.DataFrame()
 
-    def getHTML(self):
-        driver = webdriver.Firefox()
-        driver.get(self.url)
-        time.sleep(15)
-        self.html = driver.page_source
+    def parse_HTML(self):
+        driver = uc.Chrome()
+        with driver:
+            for url in self.url_list:
+                driver.get(url)
+                time.sleep(2)
+                self.read_HTML(driver.page_source)
+                time.sleep(2)
         driver.quit()
-        with open("savedHTML.txt", "w") as f:
-            f.write(self.html)
 
-    def readHTML(self, name):
-        with open(name, "r") as f:
-            self.html = f.read()
-        self.soup = BeautifulSoup(self.html, "html.parser")
-        aTags = self.soup.find_all("a", {"class": "times-shared facebook"})
-        self.articles = []
-        self.media = []
 
-        outlets = self.soup.find_all("div", {"class": "news-story-byline"})
-        for media in range(len(outlets)):
-            self.media.append(outlets[media].find_all("a")[-1].text)
+    def read_HTML(self, page_source=None):
+        if page_source is None:
+            with open('savedHTML.txt', 'r') as f:
+                page_source = f.read()
+
+
+        soup = BeautifulSoup(page_source, "html.parser")
+        aTags = soup.find_all("a", {"class": "times-shared facebook"})
+
+        ####VARIABLES TO BE PULLED FROM HTML####
+        articles = []
+        medias = {}
+        most_recent_date = None
+        #########################################
+
+        outlets = soup.find_all("div", {"class": "news-story-byline"})
+        for outlet in range(len(outlets)):
+            media = outlets[outlet].find_all("a")[-1].text
+            if media in medias:
+                medias[media] += 1
+            else:
+                medias[media] = 1
 
         for article in range(0, len(aTags), 2):
-            self.articles.append(aTags[article].get("data-description"))
+            articles.append(aTags[article].get("data-description"))
 
-        self.media = set(self.media)
+        most_recent_date = soup.find_all("a", {"class": "timeago"})[0].text
+        self.analysis(articles)
+
+    def analysis(self, articles):
+
+
+        print(articles)
+
+
+
 
     def getArticles(self):
         return self.articles
@@ -42,11 +67,6 @@ class Muckrack:
 
 
 
-
-
-
-
 if __name__ == '__main__':
-    test = Muckrack("https://muckrack.com/joseph-masha/articles")
-    test.readHTML("savedHTML.txt")
-    print(test.getMedia())
+    muck = Muckrack("https://muckrack.com/joseph-masha/articles")
+    muck.read_HTML()
