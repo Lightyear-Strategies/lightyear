c61cee70dedb96d01d64149ac6aa2b6b7e3651a6
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-01-04 13:56:16 +0300

    searching, sorting (generally), and table view work well

--- flask/flaskMain.py
+++ flask/flaskMain.py
@@ -9,7 +9,6 @@ from wtforms.validators import DataRequired, Email
 from flask_wtf.file import FileField, FileAllowed
 
 from flask_sqlalchemy import SQLAlchemy
-#import sqlalchemy
 
 import operator
 
@@ -40,7 +39,7 @@ celery = make_celery(app)
 bootstrap = Bootstrap(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'HarosDB.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -65,8 +64,6 @@ class NameForm(FlaskForm):
 
 @app.route('/table')
 def serveTable():
-    #Haros = getDBHaros()
-    #users = db.session.query(Haros).all()
     return render_template('server_table.html', title='Server-Driven Table')
 
 #@timethis
@@ -74,86 +71,89 @@ def addDBData(file):
     # Read file into dataframe
     csv_data = pd.read_csv(file.name)
 
+    # Removing white space in headers
+    csv_data.columns = csv_data.columns.str.replace(' ', '')
+
+    # Dropping the first column which is unnamed index
+    csv_data.drop('Unnamed:0', axis=1, inplace=True)
+
     # Load data to database
-    csv_data.to_sql(name='haros', con=db.engine, index=True, if_exists='append') #name = tableName
+    csv_data.to_sql(name='haros', con=db.engine, index=True, if_exists='append')
 
 def getDBHaros():
     Haros = db.Table('haros', db.metadata, autoload=True, autoload_with=db.engine)
-    #results = db.session.query(haros).all()
-   # for column in Haros.columns: print(column)
-    for col in list(Haros.columns):
-        if col.name == "Category":
-            print(col)
-
-    #print(list(Haros.columns))
     return Haros
 
 #sorting table contents
-@app.route('/api/data')
+@app.route('/api/serveHaros')
 def data():
     Haros = getDBHaros()
-    query = db.session.query(Haros).all()
+    print(Haros.columns)
+    query = db.session.query(Haros) #.all()
     #print(query)
 
     # search filter
     search = request.args.get('search[value]')
     if search:
         query = query.filter(db.or_(
-            Haros.name.like(f'%{search}%'),
+            Haros.columns.Category.like(f'%{search}%'),
+            Haros.columns.Deadline.like(f'%{search}%'),
+            Haros.columns.Email.like(f'%{search}%'),
+            Haros.columns.MediaOutlet.like(f'%{search}%'),
+            Haros.columns.Name.like(f'%{search}%'),
+            Haros.columns.Requirements.like(f'%{search}%'),
+            Haros.columns.Summary.like(f'%{search}%')
         ))
 
-    total_filtered = len(query) #.count()
-
+    total_filtered = query.count()
 
     # sorting
     order = []
     i = 0
     while True:
+
         col_index = request.args.get(f'order[{i}][column]')
         if col_index is None:
             break
+
         col_name = request.args.get(f'columns[{col_index}][data]')
-        if col_name not in ['Category','Deadline','Email','Media Outlet','Name']:
+        if col_name not in ['Category','Deadline','Email','MediaOutlet','Name']:
             col_name = 'Category'
+
+        # checks if we want descending sorting
         descending = request.args.get(f'order[{i}][dir]') == 'desc'
 
-        ### ### ### ### ###
-        """Have not Tested this part of the code"""
+        desiredCol = getattr(Haros.columns,col_name)
+        #print(desiredCol)
+        """
         desiredCol = None
         for col in list(Haros.columns):
             if col.name == col_name:
-                #col = getattr(Haros.columns,list(Haros.columns)[j].name)
                 desiredCol = col
-        ### ### ### ### ###
-
+        """
         if descending:
             desiredCol = desiredCol.desc()
         order.append(desiredCol)
+        #print("order",order)
         i += 1
     if order:
-        print(order)
-        #print(query)
-
-        """
-        Everything breakdown here on the line below because .order_by method cant sort list
-        I was not able to find "what" .order_by sorts
-        Based on this https://collerek.github.io/ormar/queries/filter-and-sort/ , it sorts Query Sets
-        
-        """
-        query.order_by(*order) #.sort(key=operator.itemgetter('name')) # = dict(
+        #print("\n Start1",query.all(),"\n")
+        query = query.order_by(*order)
+        #print("\nStart2",query.all(),"\n")
 
 
     # pagination
     start = request.args.get('start', type=int)
     length = request.args.get('length', type=int)
-    query = query[start:start+length]
+
+    query = query.offset(start).limit(length) #query[start:start+length]
 
 
     # response
     return {
-        'data': [dict(user) for user in query],
+        'data': [dict(haro) for haro in query],
         'recordsFiltered': total_filtered,
-        'recordsTotal': len(query),
+        'recordsTotal': query.count(),
         'draw': request.args.get('draw', type=int),
     }
 
@@ -222,5 +222,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     #file1 = open('HARO_test.csv')
     #addDBData(file1)
-    #getDBData()
     app.run(debug=True)
