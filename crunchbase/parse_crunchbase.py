64626ff3ce162646af9d0bdba2038ee93b069b83
liam-murphy14 <liam.murphy137@gmail.com>
2022-01-09 14:01:33 -0600

    big anti-bot win: should have this running by tomorrow

--- crunchbase/parse_crunchbase.py
+++ crunchbase/parse_crunchbase.py
@@ -4,9 +4,8 @@ import time
 import random as r 
 
 import pandas as pd
-import undetected_chromdriver as uc
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 
 class CrunchParse():
     """a class wrapper for our script"""
@@ -22,25 +21,39 @@ class CrunchParse():
         self.un = un 
         self.pw = pw 
         self.debug = debug
-        self.companies_df = pd.DataFrame()
+        self.driver = uc.Chrome()
+    
+    def __login(self):
+        """a helper method to log the user in using the username and password instance variables
+        
+        input
+        
+        None
+        """
+        # go to login page that redirects to advanced search
+        self.driver.get(r"https://www.crunchbase.com/login?redirect_to=%2Fdiscover%2Forganization.companies")
+        time.sleep(5)
+
+        email_form = self.driver.find_element_by_name("email")
+        pass_form = self.driver.find_element_by_name("password")
+        email_form.send_keys(self.un)
+        time.sleep(1.63478)
+        pass_form.send_keys(self.pw)
+        time.sleep(1.232)
+        self.driver.find_element_by_xpath('//*[@id="mat-tab-content-0-0"]/div/login/form/button').click()
+        time.sleep(3)
+        self.driver.refresh()
 
     # TODO: make keyword options instead of dict for easier gui implementation
-    def parse(self, options : dict = None):
+    def parse(self, options : dict[str, str] = None):
         """parses the table that crunchbase generates based on specific parameters
         
         input
+
         options: a dict containing the options and their values
         """
-        # driver set up
-        chrome_options = Options()
-        #chrome_options.add_argument("--disable-extensions") # OPTIONAL
-        #chrome_options.add_argument("--disable-gpu") # NECESSARY for windows
-        #chrome_options.add_argument("--no-sandbox") # NECESSARY for linux
-        #chrome_options.add_argument("--headless") # NECESSARY for all platforms
-        driver = webdriver.Chrome()
-        driver.get("https://www.crunchbase.com/discover/organization.companies") # go straight to crunchbase advanced search
-        while True:
-            pass
+        pass
+
 if __name__ == "__main__":
     CrunchParse("unknown", "unknown", True).parse()
     
