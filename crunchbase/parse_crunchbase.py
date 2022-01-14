2ed39847d35becb0181175d5f359f620bb4a81ec
liam-murphy14 <liam.murphy137@gmail.com>
2022-01-13 21:26:29 -0500

    trying to fix crunchbase, still running into MAJOR problems, will merge after this for the sake of keeping up to date

--- crunchbase/parse_crunchbase.py
+++ crunchbase/parse_crunchbase.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 
 class CrunchParse():
     """a class wrapper for our script"""
@@ -22,7 +23,7 @@ class CrunchParse():
         self.debug = debug
         self.driver = uc.Chrome()
     
-    def __login(self):
+    def login(self):
         """a helper method to log the user in using the username and password instance variables
         
         input
@@ -30,7 +31,7 @@ class CrunchParse():
         None
         """
         # go to login page that redirects to advanced search
-        self.driver.get(r"https://www.crunchbase.com/login?redirect_to=%2Fdiscover%2Forganization.companies")
+        self.driver.get(r"https://www.crunchbase.com/login?redirect_to=%2Fsearch%2Ffunding_rounds")
         time.sleep(5)
 
         email_form = self.driver.find_element_by_name("email")
@@ -44,7 +45,7 @@ class CrunchParse():
         self.driver.refresh()
 
     # TODO: make keyword options instead of dict for easier gui implementation
-    def parse(self, options : dict[str, dict[str, str]] = None):
+    def parse(self):
         """parses the table that crunchbase generates based on specific parameters
         
         input
@@ -52,39 +53,55 @@ class CrunchParse():
         options: a dict containing the options and their values
         """
         
-        # if options == None:
-        #     print("NO OPTIONS DICT, QUITTING")
-        #     self.driver.quit()
-        #     exit()
+        # THIS IS FOR DISCOVERY, NOT QUERY BUILDER. NOT FOR USE RIGHT NOW
 
-        # get page (testing only)
-        self.driver.get(r"https://www.crunchbase.com/discover/organization.companies")
-        time.sleep(4)
+        # map_options = {"overview" : 1, "contacts" : 2, "signals" : 3, "financials" : 4, "company status" : 5, "notes, lists, tags" : 6, "partner filters" : 7}
 
-        # make everything uniform
-        # self.driver.find_element_by_id("overview").click()
+        # xpath = lambda option : f"/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/discover/page-layout/div/div/div[2]/section[1]/div[1]/mat-accordion/mat-expansion-panel[{map_options[option]}]"
+
+        # # make everything uniform
+        # self.driver.find_element_by_xpath(xpath("overview")).click()
         # time.sleep(2)
 
+        # # activate filters
+        # possible_filters = map_options.keys()
 
-        overview = self.driver.find_element_by_id("overview")
-        overview.find_element_by_id("mat-input-23").send_keys("tests")
-
-        exit()
-        # activate filters
-        possible_filters = {"overview", "contacts", "signals", "financials", "company_status", "lists_and_tags", "other"}
-        for filter_group in options.keys():
-            if filter_group not in possible_filters:
-                print("BAD FILTER GROUP NAME, QUITTING")
-                self.driver.quit()
-                exit()
-            if options[filter_group] == None:
-                continue
-            self.driver.find_element_by_id(filter_group).click()
-            time.sleep(1.2382)
+        # for filter_group in options.keys():
+        #     if filter_group not in possible_filters:
+        #         print("BAD FILTER GROUP NAME, QUITTING")
+        #         self.driver.quit()
+        #         exit()
+        #     if options[filter_group] == None:
+        #         continue
+        #     self.driver.find_element_by_xpath(xpath(filter_group)).click()
+        #     time.sleep(1.2382)
 
 
+            
+    def __anounced_date_option(self, date):
+        # date in format mm/dd/yyyy
+        self.driver.find_element_by_xpath('/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/search/page-layout/div/div/form/div[1]/div/filters/query-item-add/div/button')
+        time.sleep(3)
+        mini_window = self.driver.find_element_by_xpath('//*[@id="mat-input-7"]')
+        mini_window.send_keys("announced date")
+        time.sleep(0.54)
+        mini_window.send_keys(Keys.ENTER)
+        time.sleep(3)
+        self.driver.find_element_by_xpath('//*[@id="mat-input-14"]').send_keys(date)
+        time.sleep(4)
+        self.driver.find_element_by_xpath('//*[@id="mat-input-14"]').send_keys(Keys.ENTER)
 
+    def __in_location_option(self, location):
+        self.driver.find_element_by_xpath('/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/search/page-layout/div/div/form/div[1]/div/filters/div/div/div/query-item/div/predicate/div/div[1]/query-item-add/div/button').click()
+        time.sleep(3)
+        mini_window = self.driver.find_element_by_xpath('//*[@id="mat-input-7"]')
+        mini_window.send_keys("location")
+        time.sleep(0.54)
+        mini_window.send_keys(Keys.ENTER)
+        time.sleep(3)
+        self.driver.find_element_by_xpath('//*[@id="mat-input-8"]').send_keys(location)
+        time.sleep(0.453)
+        self.driver.find_element_by_xpath('//*[@id="mat-option-159"]')
 
 if __name__ == "__main__":
-    CrunchParse("unknown", "unknown", True).parse()
-    
+    pass    
