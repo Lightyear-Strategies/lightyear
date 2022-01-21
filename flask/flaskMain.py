f756210990ff2e69f3bf7d6994fa6c0fafef4e30
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-01-21 17:34:05 +0300

    working on Celery with Amazon SQS

--- flask/flaskMain.py
+++ flask/flaskMain.py
@@ -179,7 +179,7 @@ def parseSendEmail(path, recipients=None, extension="csv", filename=None):
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 def emailVerify(path, recipients=None, extension="csv"):
-    valid = emailValidity.emailValidation(filename=path,type=extension, debug=True, multi=False)
+    valid = emailValidity.emailValidation(filename=path,type=extension, debug=True, multi=True)
     valid.check(save=True, inplace=True)
     subjectLine = os.path.basename(path)
 
