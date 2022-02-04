caf8c677e2357dfb404912b2323b2ab98b83825a
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-02-04 18:31:13 +0300

    google auth

--- flask/googleAuth.py
+++ flask/googleAuth.py
@@ -62,7 +62,7 @@ def authorize():
     # for the OAuth 2.0 client, which you configured in the API Console. If this
     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
     # error.
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+    flow.redirect_uri = flask.url_for('g_oauth.oauth2callback', _external=True)
 
     authorization_url, state = flow.authorization_url(
         # Enable offline access so that you can refresh an access token without
