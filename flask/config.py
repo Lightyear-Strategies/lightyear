4f7f5408c120e9ee8f8f29e26c7486b31cdba4c3
Liam Murphy <mailman14736@gmail.com>
2022-02-12 18:10:18 +0000

    small fix

--- flask/config.py
+++ flask/config.py
@@ -1,14 +1,14 @@
 import os
 
 AWS_ACCESS_KEY_ID = None
-with open 'aws_access.txt' as aws_a_k:
+with open('aws_access.txt') as aws_a_k:
     AWS_ACCESS_KEY_ID = aws_a_k.read()
 AWS_SECRET_ACCESS_KEY = None
-with open 'aws_secret.txt' as aws_secret:
+with open('aws_secret.txt') as aws_secret:
     AWS_SECRET_ACCESS_KEY = aws_secret.read()
 
 FLASK_SECRET_KEY = None
-with open 'flask_secret.txt' as flask_secret:
+with open('flask_secret.txt') as flask_secret:
     FLASK_SECRET_KEY = flask_secret.read()
 
 FLASK_DIR = os.path.abspath(os.path.dirname(__file__))
