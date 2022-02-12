76e6facc5b3bf23e0b28aa4359dd7bf9aac654c8
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-02-13 01:21:28 +0300

    working on .service

--- flask/flaskrunner.sh
+++ flask/flaskrunner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps auxww | grep '[c]elery worker' | awk '{print $2}' | xargs kill
+ps auxww | grep 'celery worker' | awk '{print $2}' | xargs kill -9
 cd /home/ubuntu
 source lysenv1/bin/activate
 cd lightyear/flask
