51f915469e855516182f4f615aed0f68efb1c197
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-02-12 23:42:23 +0300

    working on .service

--- flask/flaskrunner.sh
+++ flask/flaskrunner.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu
 source lysenv1/bin/activate
 cd lightyear/flask
 celery -A flaskMain.celery worker -l INFO --detach
-sudo gunicorn --bind 0.0.0.0:80 wsgi:app
+sudo gunicorn --bind 0.0.0.0:80 -w 2 wsgi:app
