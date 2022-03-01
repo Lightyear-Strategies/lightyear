4f36dac46eb15f2e4f7431c4624b5bbbe3f9e9b1
liam-murphy14 <liam.murphy137@gmail.com>
2022-03-01 12:28:54 -0500

    removed unnecessary library that was causing an error

--- flask_app/googleAuth.py
+++ flask_app/googleAuth.py
@@ -3,7 +3,6 @@ import flask
 import requests
 import pickle
 
-import google.oauth2.credentials
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
