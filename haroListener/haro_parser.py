ea36262139265a43af1732c1b6b71dd526c805f2
Ubuntu <ubuntu@ip-172-26-4-240.ca-central-1.compute.internal>
2022-03-23 17:17:11 +0000

    fixed table

--- haroListener/haro_parser.py
+++ haroListener/haro_parser.py
@@ -147,14 +147,14 @@ class Haro:
         row_dict["Name"] = message.split("Name:")[-1].split("\n")[0].replace("\r", "").strip()
         row_dict["Category"] = message.split("Category:")[-1].split("\n")[0].replace("\r", "").strip()
         row_dict["Email"] = message.split("Email:")[-1].split("\n")[0].replace("\r", "").strip()
-        row_dict["Media Outlet"] = message.split("Media Outlet:")[-1].split("\n")[0].replace("\r", "").strip()
+        row_dict["MediaOutlet"] = message.split("Media Outlet:")[-1].split("\n")[0].replace("\r", "").strip()
         row_dict["Deadline"] = message.split("Deadline:")[-1].split("\n")[0].replace("\r", "").strip()
         row_dict["Date"] = message.split("Deadline:")[-1].split("\n")[0].replace("\r", "").split("-")[-1].strip()
         row_dict["Date"] = row_dict["Date"].split(" ")[-1] + " " + row_dict["Date"].split(" ")[0].strip()
         row_dict["Time"] = message.split("Deadline:")[-1].split("\n")[0].replace("\r", "").split("-")[0].strip()
         row_dict["Query"] = message.split("Query:")[-1].split("Requirements:")[0].replace("\r", "").replace("\n", "").strip()
         row_dict["Requirements"] = message.split("Requirements:")[-1].replace("\r", "").replace("\n", "").strip()
-        row_dict["Date Received"] = self.received
+        row_dict["DateReceived"] = self.received
         row_dict["Edition"] = ' '.join(self.subject.split(" ")[1:-1])
         row_dict["Used"] = "None"
         self.df = self.df.append(row_dict, ignore_index=True)
--- reparse.py
+++ reparse.py
@@ -2,6 +2,7 @@ import pandas as pd
 import flaskMain as fm
 from haroListener.haro_listener import HaroListener
 
+
 lis = HaroListener('george@lightyearstrategies.com')
 
 haros_list = lis.find_haro_from('2021-12-01')
@@ -12,5 +13,10 @@ def df_getter(h):
 df_list = list(map(df_getter, haros_list))
 
 big_df = pd.concat(df_list)
+print(big_df)
+big_df.drop_duplicates(subset=['Summary'], inplace=True)
+print(big_df)
+big_df.reset_index(drop=True, inplace=True)
+print(big_df)
 
-fm.addDBData(big_df)
+big_df.to_sql('haros', con=fm.db.engine, index=True, if_exists='replace')
