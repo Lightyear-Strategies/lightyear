bd8234fb8b5d8defb9916630d50b485c19bc1632
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-06-12 22:42:33 +0300

    massive restructure P.4

--- flaskMain.py
+++ flaskMain.py
@@ -23,6 +23,6 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=81, debug=False, threaded=True)
 
 
