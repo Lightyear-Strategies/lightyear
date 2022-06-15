3bee8ce6d9617f3e659869edaa00a04476117269
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-06-15 18:09:47 +0300

    updated flaskrunner to work with updated repo

--- flaskrunner.sh
+++ flaskrunner.sh
@@ -11,11 +11,11 @@ if pgrep -f 'celery worker' 2>/dev/null; then
 fi
 
 echo "starting celery"
-celery -A flaskMain.celery worker -l INFO --detach
+celery -A flask_app.scripts.EmailVerification.ev_flask_functions.celery worker -l INFO --detach
 echo "started celery"
 
 echo "starting gunicorn"
-sudo gunicorn --bind 0.0.0.0:80 -w 2 wsgi:app
+sudo gunicorn -w 3 wsgi:app
 echo "started gunicorn"
 
 echo "deactivating venv"
