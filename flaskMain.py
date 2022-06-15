aa2bbebeedea6d1106641e05a984b5d22bab2bfa
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-06-15 17:14:15 +0300

    minor edits

--- flaskMain.py
+++ flaskMain.py
@@ -29,6 +29,6 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 
