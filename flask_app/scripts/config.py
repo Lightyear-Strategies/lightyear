e58f5a21c76549e2c42fd809eef77a59c6cdce55
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-06-15 18:51:06 +0300

    updated flaskrunner to work with updated repo

--- flask_app/scripts/config.py
+++ flask_app/scripts/config.py
@@ -29,7 +29,7 @@ AWS_SERVICE_NAME = 'FlaskAppSQS-1'
 DATABASE_URI = 'sqlite:///' + os.path.join(FLASK_DIR, 'Database.sqlite3')
 CELERY_LOCAL_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 
-CELERY_WEB_BROKER_URL = 'sqs://{AWS_ACCESS_KEY_ID}:{AWS_SECRET_ACCESS_KEY}' \
+CELERY_WEB_BROKER_URL = 'sqs://{AWS_ACCESS_KEY_ID}:{AWS_SECRET_ACCESS_KEY}'\
                         '@sqs.{REGION}.amazonaws.com/{ACCOUNT}/{SERVICE_NAME}'\
     .format(
         AWS_ACCESS_KEY_ID=quote(AWS_ACCESS_KEY_ID, safe=''),
