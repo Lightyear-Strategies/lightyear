c009680f0ce27a3fc29725417b4c5ab173b08e48
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-11-22 18:50:45 -0500

    debugging

--- weekly_writers_main.py
+++ weekly_writers_main.py
@@ -42,8 +42,10 @@ if __name__ == "__main__":
         unique_links = list(journalists_db['Muckrack'].unique())
         parser = mr.Muckrack(url_list=unique_links, timeframe=days_back)
         parser.parse_HTML()
+        print(parser.df)
         try:
             grouped_by_name = parser.df.groupby('Name')
+
         except KeyError:
             # no articles for anything
             grouped_by_name = None
