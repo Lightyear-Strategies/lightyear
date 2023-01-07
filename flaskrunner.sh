17a843bd9e4bca833d79cbe20f1ab2dabc3c9619
Ubuntu <ubuntu@ip-172-26-12-148.ca-central-1.compute.internal>
2023-01-07 22:13:43 +0000

    fixed flask runner

--- flaskrunner.sh
+++ flaskrunner.sh
@@ -4,16 +4,6 @@ echo "sourcing venv"
 source venv/bin/activate
 echo "sourced venv"
 
-if pgrep -f 'celery worker' 2>/dev/null; then
-  echo "terminating celery worker "
-  pkill -9 -f 'celery worker'
-  echo "terminated celery worker"
-fi
-
-echo "starting celery"
-celery -A flask_app.scripts.EmailValidator.ev_flask_functions.celery worker -l INFO --detach
-echo "started celery"
-
 echo "starting gunicorn"
 sudo gunicorn -w 3 wsgi:app
 echo "started gunicorn"
